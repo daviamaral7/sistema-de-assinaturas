04757f547c7dd46f25a663f6fe43f39d584bdf5c
Davi <daviaika07@gmail.com>
2026-04-27 21:24:07 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/davi/sistema_de_assinaturas/SistemaDeAssinaturasApplication.java
@@ -0,0 +1,13 @@
+package com.davi.sistema_de_assinaturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAssinaturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAssinaturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davi/sistema_de_assinaturas/SistemaDeAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.sistema_de_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
